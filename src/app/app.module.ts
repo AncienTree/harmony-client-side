cf5b411bd0a7d5b54bcca344700cb5aee659a1de
Mateusz Dąbek <mate.dab@gmail.com>
2019-11-24 21:12:06 +0100

    Sprzątanie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,45 +3,24 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
-import { NavigatorComponent } from './layout/navi/navigator/navigator.component';
-import { MenuComponent } from './layout/navi/menu/menu.component';
-import { FooterComponent } from './layout/navi/footer/footer.component';
-import { TestComponent } from './layout/test/test.component';
-import { TesthttpService } from './services/testhttp.service';
-import { DashboardComponent } from './layout/dashboard/dashboard.component';
-import { UserComponent } from './layout/user/user.component';
-import { AdminComponent } from './layout/admin/admin.component';
-import { HrComponent } from './layout/hr/hr.component';
-import { RaportsComponent } from './layout/raports/raports.component';
-import { NotFoundComponent } from './layout/error-page/not-found/not-found.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './modules/material.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigatorComponent,
-    MenuComponent,
-    FooterComponent,
-    TestComponent,
-    DashboardComponent,
-    UserComponent,
-    AdminComponent,
-    HrComponent,
-    RaportsComponent,
-    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule,
+    MaterialModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
   ],
-  providers: [TesthttpService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+const material = [
+
+];
+
+@NgModule({
+  imports: [material],
+  exports: [material]
+})
+export class MaterialModule { }
