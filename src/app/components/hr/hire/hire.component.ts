df95eae6e0daca70a52680bd1d03f02c1caabeb3
Mateusz Dąbek <mate.dab@gmail.com>
2020-01-06 22:51:31 +0100

    Moduł zatrudnienie
    Działający moduł zatrudnienia

--- src/app/components/hr/hire/hire.component.ts
+++ src/app/components/hr/hire/hire.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { EmployeeService } from 'src/app/services/http/employee.service';
 import { peselValidator } from 'src/app/utiles/validators/pesel-validator';
+import { Serializer } from 'src/app/model/Serializer/serializer';
 
 @Component({
   selector: 'app-hire',
@@ -12,7 +13,7 @@ export class HireComponent implements OnInit {
 
   constructor(
     private empl: EmployeeService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
   ) { }
 
   @ViewChild('stepper', { static: false }) stepper;
@@ -30,7 +31,7 @@ export class HireComponent implements OnInit {
     lastName: ['', Validators.required],
     sex: ['', Validators.required],
     birthday: ['', Validators.required],
-    startWork: ['', Validators.required],
+    startWorkDate: ['', Validators.required],
     position: ['', Validators.required],
     contractPosition: ['', Validators.required],
     contractType: ['', Validators.required],
@@ -66,19 +67,17 @@ export class HireComponent implements OnInit {
     if (!this.checkDB) {
       this.isStepTwoCompleted = true;
       this.stepper.next();
-      // Uzupełnia pesel aby nie było potrzeby ponownego wpisywania oraz datę urodzenia
+      // Uzupełnia pesel aby nie było potrzeby ponownego wpisywania
       this.pesel.setValue(this.tempPesel);
-      this.hireForm.get('birthday').setValue(this.getBirthday());
     }
   }
 
-  private getBirthday(): string {
-
-
-    return null;
-  }
-
   public submit() {
-    console.warn(this.hireForm.value);
+    console.log(this.hireForm.value);
+
+    this.empl.create(this.hireForm.value).subscribe(x => {
+      console.log('Utworzono nowy login');
+      console.log(x);
+    });
   }
 }
