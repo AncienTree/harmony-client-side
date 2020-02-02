590375e785d783ac68b69335b7eb18fcad37bb5d
Mateusz Dąbek <m.dabek@entpoint.pl>
2020-02-02 21:00:19 +0100

    data

--- src/app/components/hr/schedule/schedule.component.ts
+++ src/app/components/hr/schedule/schedule.component.ts
@@ -3,6 +3,7 @@ import { ScheduleSummaryService } from 'src/app/services/http/schedule-summary.s
 import { MatTableDataSource, MatDialog } from '@angular/material';
 import { ScheduleRecord } from 'src/app/model/schedule-record';
 import { ScheduleEditComponent } from './schedule-edit/schedule-edit.component';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-schedule',
@@ -14,21 +15,25 @@ export class ScheduleComponent {
   displayedColumns = ['fullName', 'position', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'];
   dataSource = new MatTableDataSource();
   month = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  date = new FormControl('');
 
   constructor(
     private scheduleHttp: ScheduleSummaryService,
     public dialog: MatDialog,
   ) { }
 
-  refresh(date) {
-    this.scheduleHttp.getScheduleByMonth(date)
+  refresh() {
+    console.log(this.toDate());
+
+    this.scheduleHttp.getScheduleByMonth(this.toDate())
       .subscribe(result => {
         this.dataSource.data = result;
       });
   }
 
-  testLog(object) {
-    console.log(object);
+  toDate() {
+    // tslint:disable-next-line: max-line-length
+    return this.date.value.getFullYear() + '-' + ('0' + (this.date.value.getMonth() + 1)).slice(-2) + '-' + ('0' + this.date.value.getDate()).slice(-2);
   }
 
   scheduleDialog(name: string, schedule: ScheduleRecord): void {
@@ -42,7 +47,7 @@ export class ScheduleComponent {
       console.log('Zaktualizowano grafik');
 
       // TODO zmienić
-      this.refresh('2019-10-01');
+      this.refresh();
     });
 
   }
