cd25774609878a85f92080115a0d114039455c1d
Mateusz Dąbek <mate.dab@gmail.com>
2020-05-17 19:01:11 +0200

    Dane personalne
    Pierszy szkic formatek

--- src/app/components/hr/personal-data/details/details.component.ts
+++ src/app/components/hr/personal-data/details/details.component.ts
@@ -2,7 +2,6 @@ import { EmployeeLeave } from './../../../../model/employee-leave';
 import { EmployeeInfo } from './../../../../model/employee-info';
 import { EmployeeDetails } from './../../../../model/employee-details';
 import { Employee } from './../../../../model/employee';
-import { FormBuilder, Validators } from '@angular/forms';
 import { EmployeeLeaveService } from './../../../../services/http/employee/employee-leave.service';
 import { EmployeeDetailsService } from './../../../../services/http/employee/employee-details.service';
 import { EmployeeContactService } from './../../../../services/http/employee/employee-contact.service';
@@ -22,7 +21,6 @@ export class DetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private returnRoute: Router,
-    private fb: FormBuilder,
     private employeeHttp: EmployeeService,
     private employeeInfoHttp: EmployeeInfoService,
     private employeeContactHttp: EmployeeContactService,
@@ -37,76 +35,6 @@ export class DetailsComponent implements OnInit {
   employeeInfo: EmployeeInfo;
   employeeLeave: EmployeeLeave;
   employeeContact: EmployeeContact;
-
-  // // Formatki
-  // // Podstawowe dance
-  // employeeForm = this.fb.group({
-  //   firstName: ['', Validators.required],
-  //   lastName: ['', Validators.required],
-  //   pesel: ['', Validators.required],
-  //   sex: [''],
-  //   birthday: ['', Validators.required],
-  //   email: ['', Validators.required],
-  //   position: ['', Validators.required],
-  //   contractPosition: ['', Validators.required],
-  //   workStatus: ['', Validators.required],
-  //   contractType: ['', Validators.required],
-  //   basicUnit: ['', Validators.required],
-  //   unit: ['', Validators.required],
-  //   startWorkDate: [''],
-  //   startContractDate: [''],
-  //   endWorkDate: [''],
-  //   endContractDate: ['']
-  // });
-
-  // // Dane kontaktowe
-  // employeeContactForm = this.fb.group({
-  //   address: ['', Validators.required],
-  //   city: ['', Validators.required],
-  //   zipCode: ['', Validators.required],
-  //   phoneNumber: ['', Validators.required],
-  //   contactPhoneNumber: ['', Validators.required],
-  //   contactName: ['', Validators.required]
-  // });
-
-  // // Dane informacyjne
-  // employeeInfoForm = this.fb.group({
-  //   agreement: ['', Validators.required],
-  //   ppk: ['', Validators.required],
-  //   headphones: ['', Validators.required],
-  //   locker: ['', Validators.required],
-  //   idCard: ['', Validators.required],
-  //   parkingCard: [''],
-  //   info1: [''],
-  //   info2: [''],
-  //   info3: [''],
-  //   info4: ['']
-  // });
-
-  // // Dane dodatkowe
-  // employeeDetailsForm = this.fb.group({
-  //   ltLogin: ['', Validators.required],
-  //   ltId: ['', Validators.required],
-  //   crmLogin: ['', Validators.required],
-  //   crmAccountExpirationDate: [''],
-  //   userSection: ['', Validators.required],
-  //   userLine: ['', Validators.required],
-  //   fte: [''],
-  //   fteStart: [''],
-  //   goal1: [''],
-  //   goal2: [''],
-  //   goal3: [''],
-  //   goal4: [''],
-  //   goal5: ['']
-  // });
-
-  // // Dane urlopowe
-  // employeeLeaveForm = this.fb.group({
-  //   normal: ['', Validators.required],
-  //   uz: ['', Validators.required],
-  //   additional: ['', Validators.required],
-  //   pastYears: ['', Validators.required],
-  // });
 
   ngOnInit() {
     const id = parseInt(this.route.snapshot.paramMap.get('id'));
@@ -135,6 +63,15 @@ export class DetailsComponent implements OnInit {
 
   return() {
     this.returnRoute.navigate(['/main/hr/stan-osobowy']);
+  }
+
+  leave(){
+    const norm = this.employeeLeave.normal;
+    const uz = this.employeeLeave.uz;
+    const past = this.employeeLeave.pastYears;
+    const addi = this.employeeLeave.additional;
+
+    return norm + uz + past + addi;
   }
 
   submitEmployee() {
