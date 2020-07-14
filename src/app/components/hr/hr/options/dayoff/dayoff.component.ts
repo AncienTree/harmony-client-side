a914ece043324d7c5b8b7e56da104661bfeb794d
Mateusz Dąbek <mate.dab@gmail.com>
2020-07-14 15:13:25 +0200

    Dni wolne
    Prototyp

--- src/app/components/hr/hr/options/dayoff/dayoff.component.ts
+++ src/app/components/hr/hr/options/dayoff/dayoff.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort, MatTableDataSource } from '@angular/material';
+import { DayoffService } from 'src/app/services/http/settings/dayoff.service';
 
 @Component({
   selector: 'app-dayoff',
@@ -8,13 +10,37 @@ import { Component, OnInit } from '@angular/core';
 export class DayoffComponent implements OnInit {
 
   case;
+  year: string;
+  disableTable = false;
+  displayedColumns: string[] = ['no', 'date', 'info', 'action'];
+  dataSource;
+  isLoadingResults = false;
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
 
-  constructor() { }
+  constructor(
+    private dayOffHttp: DayoffService,
+  ) { }
 
   ngOnInit() {
   }
 
   switchCase(arg) {
     this.case = arg;
+  }
+
+  delete(day) {
+    if (confirm('Czy na pewno chcesz usunąć linię ' + day.name + '?')) {
+      this.dayOffHttp.remove(day.id).subscribe(() => {
+      });
+    }
+  }
+
+  loadYear() {
+    this.isLoadingResults = true;
+    this.dayOffHttp.getDaysByYear(this.year).subscribe(respone => {
+      this.dataSource = new MatTableDataSource(respone);
+      this.disableTable = true;
+      this.isLoadingResults = false;
+    });
   }
 }
---
+++ src/app/services/http/settings/dayoff.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DayoffService {
+  readonly url = environment.url;
+
+  constructor(private http: HttpClient) { }
+
+  public getDays(start, end): Observable<any> {
+    return this.http
+      .post(`${this.url}/setting/dayoff/between/`, {
+        start,
+        end
+      });
+  }
+
+    public getDaysByYear(year): Observable<any> {
+    return this.http
+      .get(`${this.url}/setting/dayoff/${year}`);
+  }
+
+  public create(dayoff): Observable<any> {
+    return this.http
+      .post(`${this.url}/setting/dayoff/`, dayoff, { responseType: 'text' });
+  }
+
+  public remove(id): Observable<any> {
+    return this.http
+      .delete(`${this.url}/setting/dayoff/${id}`, { responseType: 'text' });
+  }
+
+  public update(dayoff): Observable<any> {
+    return this.http
+      .patch(`${this.url}/setting/dayoff/`, dayoff, { responseType: 'text' });
+  }
+}
